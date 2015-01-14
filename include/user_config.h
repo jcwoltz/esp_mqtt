eecb3539927b9668f73008bc3bfef41f966baae5
JC Woltz <jwoltz@gmail.com>
2015-01-14 13:58:35 -0500

    add various drivers

--- include/user_config.h
+++ include/user_config.h
@@ -6,18 +6,18 @@
 
 /*DEFAULT CONFIGURATIONS*/
 
-#define MQTT_HOST			"mqtt.yourdomain.com" //or "192.168.11.1"
-#define MQTT_PORT			8440
+#define MQTT_HOST			"util1.wocspool.com" //or "192.168.11.1"
+#define MQTT_PORT			1883
 #define MQTT_BUF_SIZE		1024
 #define MQTT_KEEPALIVE		120	 /*second*/
 
-#define MQTT_CLIENT_ID		"DVES_%08X"
-#define MQTT_USER			"DVES_USER"
-#define MQTT_PASS			"DVES_PASS"
+#define MQTT_CLIENT_ID		"jch_%08X"
+#define MQTT_USER			"JCH_node1"
+#define MQTT_PASS			"abc123"
 
 
-#define STA_SSID "DVES_HOME"
-#define STA_PASS "wifipassword"
+#define STA_SSID "ocs9"
+#define STA_PASS "BasketBall4All"
 #define STA_TYPE AUTH_WPA2_PSK
 
 #define MQTT_RECONNECT_TIMEOUT 	5	/*second*/
