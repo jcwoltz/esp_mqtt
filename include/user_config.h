b8d42ad5b1e6129728104588c751019d1e9c931a
JC Woltz <jwoltz@gmail.com>
2015-01-18 09:53:53 -0500

    sync author chnges

--- include/user_config.h
+++ include/user_config.h
@@ -1,18 +1,18 @@
 #ifndef _USER_CONFIG_H_
 #define _USER_CONFIG_H_
 
-#define CFG_HOLDER	0x00FF55A4
+#define CFG_HOLDER	0x00FF55A4	/* Change this value to load default configurations */
 #define CFG_LOCATION	0x3C	/* Please don't change or if you know what you doing */
 
 /*DEFAULT CONFIGURATIONS*/
 
-#define MQTT_HOST			"104.200.29.35" //or "192.168.11.1"
+#define MQTT_HOST			"util1.wocspool.com" //or "mqtt.yourdomain.com"
 #define MQTT_PORT			1883
 #define MQTT_BUF_SIZE		1024
 #define MQTT_KEEPALIVE		120	 /*second*/
 
-#define MQTT_CLIENT_ID		"jch_%08X"
-#define MQTT_USER			"JCH_node1"
+#define MQTT_CLIENT_ID		"DVES_%08X"
+#define MQTT_USER			"jch_node1"
 #define MQTT_PASS			"abc123"
 
 
@@ -23,7 +23,9 @@
 #define MQTT_RECONNECT_TIMEOUT 	5	/*second*/
 
 #define CLIENT_SSL_ENABLE
+
 #define DEFAULT_SECURITY	0
-#define QUEUE_BUFFER_SIZE	2048
+
+#define QUEUE_BUFFER_SIZE		 		2048
 
 #endif
--- user/user_main.c
+++ user/user_main.c
@@ -36,94 +36,9 @@
 #include "debug.h"
 #include "gpio.h"
 #include "user_interface.h"
-//#include "driver/dht22.h"
-#include "driver/gpio16.h"
+#include "mem.h"
 
 MQTT_Client mqttClient;
-#define DELAY 4000 /* milliseconds */
-#define user_procTaskPrio        3
-#define user_procTaskQueueLen    1
-
-LOCAL os_event_t	user_procTaskQueue[user_procTaskQueueLen];
-LOCAL os_timer_t	dht22_timer;
-//scl gpio14 pin17
-//sda gpio2 pin20
-//dht gpio12 pin 16
-
-#define LED1_GPIO 4	//pin 22 nothing else attached
-#define LED1_GPIO_MUX PERIPHS_IO_MUX_GPIO4_U
-#define LED1_GPIO_FUNC FUNC_GPIO4
-
-#define LED2_GPIO 5	//pin 11 nothing else attached
-#define LED2_GPIO_MUX PERIPHS_IO_MUX_GPIO5_U
-#define LED2_GPIO_FUNC FUNC_GPIO5
-
-#define LED3_GPIO 15	//pin 19 2k gnd
-#define LED3_GPIO_MUX PERIPHS_IO_MUX_MTDO_U
-#define LED3_GPIO_FUNC FUNC_GPIO15
-
-#define LED4_GPIO 13	//pin 18 nothing else attached
-#define LED4_GPIO_MUX PERIPHS_IO_MUX_MTCK_U
-#define LED4_GPIO_FUNC FUNC_GPIO13
-
-#define LED5_GPIO 0	//pin 21 2k pullup
-#define LED5_GPIO_MUX PERIPHS_IO_MUX_GPIO0_U
-#define LED5_GPIO_FUNC FUNC_GPIO0
-
-#define LED_GPIO LED5_GPIO
-#define LED_GPIO_MUX LED5_GPIO_MUX
-#define LED_GPIO_FUNC LED5_GPIO_FUNC
-
-
-
-static void user_procTask(os_event_t *events);
-static volatile os_timer_t some_timer;
-
-#ifdef __DHT22_H__
-LOCAL void ICACHE_FLASH_ATTR dht22_cb(void *arg)
-{
- struct dht_sensor_data* r = DHTRead();
- float lastTemp = r->temperature;
- float lastHum = r->humidity;
- char str[64];
-
- if(r->success)
- {
- INFO("Temperature: %d.%d *C, Humidity: %d.%d %%\r\n", (int)(lastTemp),(int)((lastTemp - (int)lastTemp)*100), (int)(lastHum),(int)((lastHum - (int)lastHum)*100));
- os_sprintf(str,"%d.%d", (int)(lastTemp),(int)((lastTemp - (int)lastTemp)*100));
- MQTT_Publish(&mqttClient, "sensor/node1/dht/t", str, os_strlen(str), 0, 0);
- }
- else
- {
- INFO("Error reading temperature and humidity\r\n");
- }
-}
-#endif
-
-void some_timerfunc(void *arg)
-{
-    //Do blinky stuff on LED4
-    if (GPIO_REG_READ(GPIO_OUT_ADDRESS) & BIT13)
-    {
-        //Set GPIO13 to LOW
-	//GPIO_OUTPUT_SET(LED4_GPIO, 0);
-        gpio_output_set(0, BIT13, BIT13, 0);
-    }
-    else
-    {
-        //Set GPIO13 to HIGH
-	//GPIO_OUTPUT_SET(LED4_GPIO, 1);
-        gpio_output_set(BIT13, 0, BIT13, 0);
-    }
-}
-
-//Do nothing function
-static void ICACHE_FLASH_ATTR
-user_procTask(os_event_t *events)
-{
-    os_delay_us(10);
-}
-
 
 void wifiConnectCb(uint8_t status)
 {
@@ -135,11 +50,14 @@ void mqttConnectedCb(uint32_t *args)
 {
 	MQTT_Client* client = (MQTT_Client*)args;
 	INFO("MQTT: Connected\r\n");
-	INFO("MQTT: Starting subscription\r\n");
-	MQTT_Subscribe(client, "sensor/node1/control/led1", 2);
-	//uncomment next line for second subscription, but the first uses wildcard
-	MQTT_Subscribe(client, "sensor/node1/control/led2", 2);
-	//MQTT_Publish(client, "sensor/node1/a", "online", 6, 0, 0);
+	MQTT_Subscribe(client, "sensor/node1/control/#", 0);
+	//MQTT_Subscribe(client, "/mqtt/topic/1", 1);
+	//MQTT_Subscribe(client, "/mqtt/topic/2", 2);
+
+	//MQTT_Publish(client, "/mqtt/topic/0", "hello0", 6, 0, 0);
+	//MQTT_Publish(client, "/mqtt/topic/1", "hello1", 6, 1, 0);
+	//MQTT_Publish(client, "/mqtt/topic/2", "hello2", 6, 2, 0);
+
 }
 
 void mqttDisconnectedCb(uint32_t *args)
@@ -156,7 +74,9 @@ void mqttPublishedCb(uint32_t *args)
 
 void mqttDataCb(uint32_t *args, const char* topic, uint32_t topic_len, const char *data, uint32_t data_len)
 {
-	char topicBuf[64], dataBuf[64];
+	char *topicBuf = (char*)os_zalloc(topic_len+1),
+			*dataBuf = (char*)os_zalloc(data_len+1);
+
 	MQTT_Client* client = (MQTT_Client*)args;
 
 	os_memcpy(topicBuf, topic, topic_len);
@@ -165,88 +85,26 @@ void mqttDataCb(uint32_t *args, const char* topic, uint32_t topic_len, const cha
 	os_memcpy(dataBuf, data, data_len);
 	dataBuf[data_len] = 0;
 
-	INFO("MQTT topic: %s, data: %s \r\n", topicBuf, dataBuf);
-
-	/* Echo back to /echo channel*/
-	//MQTT_Publish(client, "/echo", dataBuf, data_len, 0, 0);
-
-	if(0 == os_strcmp(topicBuf,"sensor/node1/control/led1")) {
-	 INFO("LED1 sub\r\n");
-	 if (0 == os_strcmp(dataBuf,"on"))
-	 {
-	  INFO("LED1 sub data high\r\n");
-	  //Set GPIO2 to HIGH
-	  //gpio_output_set(BIT2, 0, BIT2, 0);
-	  GPIO_OUTPUT_SET(LED1_GPIO, 1);
-	 } else if (0 == os_strcmp(dataBuf,"off"))
-	 {
-	  INFO("LED1 sub data low\r\n");
-	  //Set GPIO2 to LOW
-	  //gpio_output_set(0, BIT2, BIT2, 0);
-	  GPIO_OUTPUT_SET(LED1_GPIO, 0);
-	 } else {
-	  INFO("\r\n Unable to parse data and set led1: \r\n");
-	 }
-	} else if(0 == os_strcmp(topicBuf,"sensor/node1/control/led2")) {
-	 INFO("LED2 sub\r\n");
-	 if (0 == os_strcmp(dataBuf,"on"))
-	 {
-	  INFO("LED2 sub data high\r\n");
-	  //Set GPIO2 to HIGH
-	  //gpio_output_set(BIT2, 0, BIT2, 0);
-	  GPIO_OUTPUT_SET(LED2_GPIO, 1);
-	 } else if (0 == os_strcmp(dataBuf,"off"))
-	 {
-	  INFO("LED2 sub data low\r\n");
-	  //Set GPIO2 to LOW
-	  //gpio_output_set(0, BIT2, BIT2, 0);
-	  GPIO_OUTPUT_SET(LED2_GPIO, 0);
-	 } else {
-	  INFO("\r\n Unable to parse data and set led2: \r\n");
-	 }
-	}
-
+	INFO("Receive topic: %s, data: %s \r\n", topicBuf, dataBuf);
+	os_free(topicBuf);
+	os_free(dataBuf);
 }
 
 
 void user_init(void)
 {
 	uart_init(BIT_RATE_115200, BIT_RATE_115200);
-	gpio_init();
-	PIN_FUNC_SELECT(LED1_GPIO_MUX, LED1_GPIO_FUNC);
-	PIN_FUNC_SELECT(LED2_GPIO_MUX, LED2_GPIO_FUNC);
-	PIN_FUNC_SELECT(LED3_GPIO_MUX, LED3_GPIO_FUNC);
-	PIN_FUNC_SELECT(LED4_GPIO_MUX, LED4_GPIO_FUNC);
-	PIN_FUNC_SELECT(LED5_GPIO_MUX, LED5_GPIO_FUNC);
-	GPIO_OUTPUT_SET(LED1_GPIO, 0);
-	GPIO_OUTPUT_SET(LED2_GPIO, 0);
-	GPIO_OUTPUT_SET(LED4_GPIO, 0);
-	//DHTInit(DHT11, 2000);
-
-        //Set GPIO2 to output mode
-        //PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
-        //Set GPIO2 low
-        //gpio_output_set(0, BIT2, BIT2, 0);
-
-        //Disarm timer
-        os_timer_disarm(&some_timer);
-        //Setup timer
-        os_timer_setfn(&some_timer, (os_timer_func_t *)some_timerfunc, NULL);
-        //Arm the timer
-        //&some_timer is the pointer
-        //1000 is the fire time in ms
-        //0 for once and 1 for repeating
-	os_timer_arm(&some_timer, 50, 1);
-        //Start os task
-        system_os_task(user_procTask, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
-
-
-	//os_delay_us(4000000);
+	os_delay_us(1000000);
 
 	CFG_Load();
 
-	MQTT_InitConnection(&mqttClient, sysCfg.mqtt_host, sysCfg.mqtt_port, SEC_NONSSL);
-	MQTT_InitClient(&mqttClient, sysCfg.device_id, sysCfg.mqtt_user, sysCfg.mqtt_pass, sysCfg.mqtt_keepalive);
+	MQTT_InitConnection(&mqttClient, sysCfg.mqtt_host, sysCfg.mqtt_port, sysCfg.security);
+	//MQTT_InitConnection(&mqttClient, "192.168.11.122", 1880, 0);
+
+	MQTT_InitClient(&mqttClient, sysCfg.device_id, sysCfg.mqtt_user, sysCfg.mqtt_pass, sysCfg.mqtt_keepalive, 1);
+	//MQTT_InitClient(&mqttClient, "client_id", "user", "pass", 120, 1);
+
+	//MQTT_InitLWT(&mqttClient, "/lwt", "offline", 0, 0);
 	MQTT_OnConnected(&mqttClient, mqttConnectedCb);
 	MQTT_OnDisconnected(&mqttClient, mqttDisconnectedCb);
 	MQTT_OnPublished(&mqttClient, mqttPublishedCb);
@@ -255,11 +113,4 @@ void user_init(void)
 	WIFI_Connect(sysCfg.sta_ssid, sysCfg.sta_pwd, wifiConnectCb);
 
 	INFO("\r\nSystem started ...\r\n");
-
-#ifdef __DHT22_H__
-	os_timer_disarm(&dht22_timer);
-	os_timer_setfn(&dht22_timer, (os_timer_func_t *)dht22_cb, (void *)0);
-	os_timer_arm(&dht22_timer, DELAY, 1);
-#endif
-
 }
